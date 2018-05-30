1d590409804ea0083e865cf17765256a150f366d
Jeon <sodtms@naver.com>
2018-05-30 14:59:47 +0900

    add BGSingleton class
    GoF 디자인 패턴 적용
    멀티스레드 safe
    C++11

---
+++ BGSingleton.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <mutex>
+
+/**
+ * std::once_flag는 초기화 하지 않으며 std::call_once와 연동되어 멀티스레드 환경에서
+ * 단 한번만 실행되는 것을 운영체제 차원에서 보장해 주도록 구현되었다.
+ * 컴파일러가 만들어 주는 클래스의 복사 생성자나 할당 연산자를 사용시 깊은 복사(Deep copy)를
+ * 수행하는 복사 시맨틱(Copy Semantics)이 일어난다. 하지만 벡터와 같은 컨테이너에 작업을
+ * 수행하면 원본 객체는 사용하지 않게 되어 낭비가 발생한다. 소유권 이전은 얕은 복사(Shallow copy)이후
+ * 원본을 더미 객체로 만들어 소유권을 이전시키는 이동 시맨틱(Move Semantics)이 일어나는데,
+ * 대표적으로 이동 생성자나 unique_ptr이 있다. unique_ptr에는 복사 생성자나 할당 연산자가 없기 때문에
+ * std::move()를 사용하거나 reset() 메소드를 이용해 객체를 할당하며 스마트 포인터기 때문에 자원해제에 안전하다.
+*/
+
+class BGSingleton
+{
+private:
+	static std::once_flag onceflag_;
+	static std::unique_ptr<BGSingleton> instance_;
+	
+protected:
+	BGSingleton() = default;
+	BGSingleton(const BGSingleton&) = default;
+	BGSingleton& operator=(const BGSingleton&) = delete;
+
+public:
+	static BGSingleton* Instance() {
+		std::call_once(onceflag_, []() {
+			instance_.reset(new BGSingleton);
+		});
+		return instance_.get();
+	}
+};
+
+std::once_flag BGSingleton::onceflag_;
+std::unique_ptr<BGSingleton> BGSingleton::instance_{ nullptr };
