bd180a30290d8310d44fbc0e66a39338c0bd5d60
Jeon <sodtms@naver.com>
2018-05-31 18:31:13 +0900

    [1.02] 로그 : BGLog 로그레벨 얻어오는 함수 추가

--- BGLog.h
+++ BGLog.h
@@ -15,6 +15,7 @@ public:
 public:
 	bool Valid() { return m_logLevel != ELogLevel::NONE; }
 	bool IsStopRequest() { if (m_logLevel == ELogLevel::INFO && m_content == "STOP") return true; return false; }
+	ELogLevel GetLevel() { return m_logLevel; }
 
 private:
 	ELogLevel m_logLevel;
