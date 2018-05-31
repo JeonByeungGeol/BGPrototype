6648c26c24d68680acee499c85855129d3874dd3
Jeon <sodtms@naver.com>
2018-05-31 15:54:33 +0900

    [1.02] 로그 : BGLog 객체에 로그레벨, 내용 멤버 추가
    -로그 레벨은 초기에 NONE으로 설정
    -Valid멤버 함수로 NONE이면 fale 리턴

---
+++ BGLog.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "BGLog.h"
+
+
+BGLog::BGLog()
+{
+}
+
+
+BGLog::~BGLog()
+{
+}
--- BGLog.h
+++ BGLog.h
@@ -1,8 +1,19 @@
 #pragma once
+
+#include "BGLogEnum.h"
+
 class BGLog
 {
 public:
-	BGLog();
+	BGLog() : m_logLevel(ELogLevel::NONE) {};
 	~BGLog();
+
+
+public:
+	bool Valid() { return m_logLevel != ELogLevel::NONE; }
+
+private:
+	ELogLevel m_logLevel;
+	std::string m_content;
 };
 
