8adbabe97316d0aa240d2894b1ee3ed6d75b2bca
Jeon <sodtms@naver.com>
2018-06-01 12:29:00 +0900

    [1.02] 로그 : ELogLevel 수정 : enum -> enum class 로 개선, TRACE -> BG_TRACE로 개선
    -열거자중 ERROR가 "windows.h"에 포함되어있는 ERROR와 중복되어 개선

--- BGLogEnum.h
+++ BGLogEnum.h
@@ -1,14 +1,14 @@
 #pragma once
 
 
-enum ELogLevel
+enum class ELogLevel
 {
-	NONE,
-	TRACE,
-	DEBUG,
-	INFO,
-	WARNING,
-	ERROR,
-	FATAL,
-	//EXTRACT_DATA
+	BG_NONE,
+	BG_TRACE,
+	BG_DEBUG,
+	BG_INFO,
+	BG_WARNING,
+	BG_ERROR,
+	BG_FATAL,
+	//BG_EXTRACT_DATA_1
 };
