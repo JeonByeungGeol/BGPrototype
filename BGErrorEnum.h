b6fde3b60d735749a8ca09072b0acd588fde9be8
Jeon <sodtms@naver.com>
2018-08-01 10:44:56 +0900

    [1.04] 에러 : BGErrorEnum 객체 'BG_ERROR' namespace로 관리 -> class 객체로 관리
    공통적으로 관리 할수 있도록 하기 위한 처리
    예를들어, BGUtil에 인자로 BG_ERROR를 받는 함수를 만들 수 있음

--- BGErrorEnum.h
+++ BGErrorEnum.h
@@ -1,11 +1,16 @@
 #pragma once
 
-namespace BG_ERROR {
+class BG_ERROR {
+public:
 	enum class Common {
 		BG_NONE,	// 정상
 		BG_ERROR,	// 에러	
 	};
 
-}
+	enum class Http {
+		BG_NONE,	// 정상
+		BG_ERROR,	// 에러
+	};
+};
 
 
