2f352f32001bebb923cbfce29648e2c633f30c2f
Jeon <sodtms@naver.com>
2018-08-01 10:49:23 +0900

    [1.04] 에러 : enum class에 정의된 에러코드들을 정수형으로 반환할 수 있는 기능 추가

--- BGErrorEnum.h
+++ BGErrorEnum.h
@@ -11,6 +11,9 @@ public:
 		BG_NONE,	// 정상
 		BG_ERROR,	// 에러
 	};
+
+	static int ErrorCode(Common error) { return static_cast<int>(error); }
+	static int ErrorCode(Http error) { return static_cast<int>(error); }
 };
 
 
